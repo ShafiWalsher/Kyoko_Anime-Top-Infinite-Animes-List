71082121a30fe6ecdd1b664fd03933fc5b61ff6c
Shafi Walsher <shafiwalsher@gmail.com>
2024-01-03 14:52:47 +0530

    Added Fevicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anime Vault",
+  title: "Kyoko Anime",
   description: "Your favorite anime, all in one place.",
 };
 
